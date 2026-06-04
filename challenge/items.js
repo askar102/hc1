15a7adbd82c7fd5377564d69be939d1cbb10c20b
askar102 <sergey.bolotny@icloud.com>
2026-06-04 17:30:25 +0500

    added: photo select

--- challenge/items.js
+++ challenge/items.js
@@ -8,9 +8,12 @@ for (const task of taskGrid.children) {
         let isPressed = ariaIsPressed(task.getAttribute('aria-pressed'));
         console.log(isPressed);
         task.setAttribute('aria-pressed', String(!isPressed));
+
+        const taskImageWrapper = task.querySelector('.task-image .wrapper');
+
+        changeVisual(taskImageWrapper, isPressed);
     });
 }
-
 
 function ariaIsPressed(task_attribute) {
     if (task_attribute === "true") {
@@ -21,3 +24,54 @@ function ariaIsPressed(task_attribute) {
         return false;
     }
 }
+
+function changeVisual(taskImageWrapper, isPressed) {
+    if (!taskImageWrapper) return;
+
+    childs = [taskImageWrapper, ...taskImageWrapper.querySelectorAll('*')];
+
+    childs.forEach(element => {
+        if (isPressed) {
+            element.style['width'] = "100.8px";
+            element.style['height'] = "100.8px";
+            element.style['left'] = "50%";
+            element.style['top'] = "50%";
+            element.style['margin-top'] = "-50.4px";
+            element.style['margin-left'] = "-50.4px";
+        }
+        else {
+            element.style['width'] = "120px";
+            element.style['height'] = "120px";
+            element.style['left'] = "50%";
+            element.style['top'] = "50%";
+            element.style['margin-top'] = "-60px";
+            element.style['margin-left'] = "-60px";
+        }
+    });
+}
+
+// wrapper
+// width: 100.8px;
+//   height: 100.8px;
+//   overflow: hidden;
+//   border-radius: 2px;
+//   transition: 0.1s cubic-bezier(0.05, 0.55, 0.5, 0.99);
+//   position: relative;
+//   top: 50%;
+//   left: 50%;
+//   margin-top: -50.4px;
+//   margin-left: -50.4px;
+//   background-color: rgb(245, 245, 245);
+
+// default
+// width: 120px;
+//   height: 120px;
+//   overflow: hidden;
+//   border-radius: 2px;
+//   transition: 0.1s cubic-bezier(0.05, 0.55, 0.5, 0.99);
+//   position: relative;
+//   top: 50%;
+//   left: 50%;
+//   margin-top: -60px;
+//   margin-left: -60px;
+//   background-color: rgb(245, 245, 245);
